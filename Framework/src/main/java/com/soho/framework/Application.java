08271d5cfb12b2893e54f4bfd01a3f822cc2ce05
ZhongChongtao <2593364535@qq.com>
2017-03-19 16:37:57 +0800

    Application.java

--- Framework/src/main/java/com/soho/framework/Application.java
+++ Framework/src/main/java/com/soho/framework/Application.java
@@ -2,10 +2,15 @@ package com.soho.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * Created by ZhongChongtao on 2017/3/18.
  */
 @SpringBootApplication
+@EnableWebMvc
+@CrossOrigin("*.*")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
